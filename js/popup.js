c174b03fd36b130c860d5b4227f3947db2df4363
Trent Brew <tbrew1023@gmail.com>
2022-09-04 15:08:29 -0500

    update css

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,8 @@
 $(() => {
 	get()
 
+	$("#alias").focus()
+
 	$("#new_alias").submit(e => {
 		var alias = $("#alias").val().replace(/[^\w]/g, '')
 		var url = $("#url").val()
@@ -10,6 +12,7 @@ $(() => {
 			return;
 		}
 		set(alias, url)
+		window.close()
 		return false
 	})
 })
@@ -53,15 +56,15 @@ function remove(alias) {
 
 function insert(alias, url) {
 	$("#aliases").append(`
-		<div class="flex gap-3 mb-4 w-full">
+		<li class="flex gap-3 mb-4 w-full justify-start">
 
-			<a class="badge badge-error badge-lg px-1 hover:opacity-75 cursor-pointer duration-0" id="${alias}">
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-			</a>
+		<!-- remove alias -->
+			<a class="badge badge-error badge-lg px-1 hover:opacity-75 cursor-pointer !duration-0" id="${alias}"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg></a>
 
+			<!-- alias item -->
 			<div class="badge badge-lg badge-secondary">${alias}</div><div class="badge badge-lg max-w-full align-left truncate opacity-50">${url}</div>
 
-		</div>
+		</li>
 	`)
 	$(`#${alias}`).click(e => {
 		remove(alias)

21fc4a4e27012faa5ccf6a94c8d6f8bb84788251
Trent Brew <tbrew1023@gmail.com>
2022-09-04 02:16:17 -0500

    genesis

---
+++ js/background.js
@@ -0,0 +1,59 @@
+var aliases = {}
+
+chrome.storage.sync.get(null, obj => {
+  for (o in obj) aliases[o] = obj[o]
+})
+
+var re = /[\d\s\+\-=\(\)\*]+/g
+var link = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
+var search_url = /https:\/\/www\.google\.com\/search\?q=(\w+)&/
+
+// On input changed, call this
+
+chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+    var suggestions = []
+    for (key in aliases) {
+      if (key.startsWith(text) || text == "") {
+        var desc = `<match>${text}: </match><dim>${key} → </dim><url>${aliases[key]}</url>`
+        suggestions.push({content: aliases[key], description: desc});
+      }
+    }
+    if (text.match(re)) {
+      var result = eval(text).toString()
+      chrome.omnibox.setDefaultSuggestion({description: `<match>= </match><url>${result}</url>`})
+    }
+    else if (suggestions.length > 0) {
+      var first = suggestions.splice(0, 1)[0]
+      chrome.omnibox.setDefaultSuggestion({description: first['description']})
+    }
+    else {
+      chrome.omnibox.setDefaultSuggestion({description: `<match>${text}: </match><dim> - Google Search</dim>`})
+    }
+    suggest(suggestions)
+})
+
+// This event is fired with the user accepts the input in the omnibox.
+
+chrome.omnibox.onInputEntered.addListener(text => {
+    if (text in aliases) {
+      chrome.tabs.update({ url: aliases[text] })
+    }
+    else if (text.match(link)) {
+      chrome.tabs.update({ url: text } )
+    }
+    else if (text.match(re)) {
+      var result = eval(text).toString();
+      chrome.tabs.update({url: `https://google.com/search?q=${result}`})
+    }
+    else {
+      chrome.tabs.update({url: `https://google.com/search?q=${text}`})
+    }
+})
+
+// Starting input
+
+chrome.omnibox.onInputStarted.addListener(() => {
+  chrome.storage.sync.get(null, obj => {
+    for (o in obj) aliases[o] = obj[o]
+  })
+})
---
+++ js/popup.js
@@ -0,0 +1,70 @@
+$(() => {
+	get()
+
+	$("#new_alias").submit(e => {
+		var alias = $("#alias").val().replace(/[^\w]/g, '')
+		var url = $("#url").val()
+		if (alias == "" || url == "") {
+			e.preventDefault()
+			alert("You must enter an alias and url...")
+			return;
+		}
+		set(alias, url)
+		return false
+	})
+})
+
+function set(alias, url) {
+	var obj = {}
+	obj[alias] = url
+	chrome.storage.sync.set(obj, () => {
+  	$("#alias").val("")
+		$("#url").val("")
+	})
+}
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    for (key in changes) {
+      var storageChange = changes[key]
+      if (storageChange.newValue != null) {
+      	insert(key, storageChange.newValue)
+      }
+    }
+})
+
+function get() {
+	chrome.storage.sync.get(null, obj => {
+		for (o in obj) insert(o, obj[o])
+	})
+}
+
+function clear() {
+	chrome.storage.sync.clear(() => {
+		console.log('cleared!')
+	})
+	$("#aliases").html("")
+}
+
+function remove(alias) {
+	chrome.storage.sync.remove(alias, () => {
+		$(`#${alias}`).parent().remove()
+	})
+}
+
+function insert(alias, url) {
+	$("#aliases").append(`
+		<div class="flex gap-3 mb-4 w-full">
+
+			<a class="badge badge-error badge-lg px-1 hover:opacity-75 cursor-pointer duration-0" id="${alias}">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+			</a>
+
+			<div class="badge badge-lg badge-secondary">${alias}</div><div class="badge badge-lg max-w-full align-left truncate opacity-50">${url}</div>
+
+		</div>
+	`)
+	$(`#${alias}`).click(e => {
+		remove(alias)
+		return false
+	})
+}

d19c0e159f1271485d0e1f9d1533895e51ab4f2d
Trent Brew <tbrew1023@gmail.com>
2022-09-05 04:07:10 -0500

    jquery surgery

--- js/popup.js
+++ js/popup.js
@@ -1,56 +1,67 @@
-new Vue({
-  el: "#popup",
-  data: () => {
-    return {
-      alias: {
-        name: '',
-        url: '',
-        status: '',
-        placeholder: {
-          name: 'alias',
-          url: 'url',
-        }
-      },
-      aliases: [],
-    }
-  },
-  mounted: function() {
-    this.$refs.alias.focus()
-    this.getAliases()
-    chrome.storage.onChanged.addListener((changes, namespace) => {
-      for (key in changes) {
-        let storageChange = changes[key]
-        if (storageChange.newValue != null) {
-          this.aliases.push(key, storageChange.newValue)
-        }
-      }
-    })
-  },
-  methods: {
-    getAliases() {
-      chrome.storage.sync.get(null, aliases => {
-        for (alias in aliases) {
-          this.aliases.push({ name: alias, url: aliases[alias] })
-        }
-      })
-    },
-    setAlias(alias, url) {
-      this.aliases[alias] = url
-      chrome.storage.sync.set(this.aliases)
-    },
-    removeAlias(alias) {
-      chrome.storage.sync.remove(alias)
-      this.getAliases()
-    },
-    submit() {
-      const alias = this.alias.name.replace(/[^\w]/g, '')
-      const url = this.alias.url
-      if (!alias || !url) {
-        alert('Please enter an alias and URL')
-      }
-      this.setAlias(alias, url)
-      window.close()
-      return false
-    },
-  },
+$(() => {
+	get()
+
+	$("#new_alias").submit(e => {
+		var alias = $("#alias").val().replace(/[^\w]/g, '')
+		var url = $("#url").val()
+		if (alias == "" || url == "") {
+			e.preventDefault()
+			alert("You must enter an alias and url...")
+			return;
+		}
+		set(alias, url)
+		return false
+	})
 })
+
+function set(alias, url) {
+	var obj = {}
+	obj[alias] = url
+	chrome.storage.sync.set(obj, () => {
+  	$("#alias").val("")
+		$("#url").val("")
+	})
+}
+
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    for (key in changes) {
+      var storageChange = changes[key]
+      if (storageChange.newValue != null) {
+      	insert(key, storageChange.newValue)
+      }
+    }
+})
+
+function get() {
+	chrome.storage.sync.get(null, obj => {
+		for (o in obj) insert(o, obj[o])
+	})
+}
+
+function clear() {
+	chrome.storage.sync.clear(() => {
+		console.log('cleared!')
+	})
+	$("#aliases").html("")
+}
+
+function remove(alias) {
+	chrome.storage.sync.remove(alias, () => {
+		$(`#${alias}`).parent().remove()
+	})
+}
+
+function insert(alias, url) {
+	$("#aliases").append(`
+		<div class="flex gap-3 mb-4 w-full">
+			<a class="badge badge-error badge-lg px-1 hover:opacity-75 cursor-pointer duration-0" id="${alias}">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+			</a>
+			<div class="badge badge-lg badge-secondary">${alias}</div><div class="badge badge-lg max-w-full align-left truncate opacity-50">${url}</div>
+		</div>
+	`)
+	$(`#${alias}`).click(e => {
+		remove(alias)
+		return false
+	})
+}
